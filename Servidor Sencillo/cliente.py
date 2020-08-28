be5a8a5712d5942d2f03925c3522fdc49530ee38
Julian Alfonso <guiaparalaensenanzadego@gmail.com>
2020-08-27 23:26:18 -0500

    Se pasaron los archivos de Servidor Sencillo  a python3 y se documentaron

--- Servidor Sencillo/cliente.py
+++ Servidor Sencillo/cliente.py
@@ -1,8 +1,20 @@
-import xmlrpclib  
-a =input(str('Ingrese el primer numero: '))
-b=input(str('Ingrese el segundo numero: '))
-proxy = xmlrpclib.ServerProxy("http://localhost:8001/")
-print "La suma es: " +str(proxy.suma(a,b))
+'''
+XML-RPC is a Remote Procedure Call method that uses XML passed via HTTP(S) as a transport. With it, a client can call methods with parameters on a remote server (the server is named by a URI) and get back structured data.
+
+'''
+#Importamos la libreria del lado del cliente
+import xmlrpc.client
+
+
+#Solicitamos los datos al usuario
+a =eval(input(str('Ingrese el primer numero: ')))
+b=eval(input(str('Ingrese el segundo numero: ')))
+
+#A ServerProxy instance is an object that manages communication with a remote XML-RPC server.The required first argument is a URI
+proxy = xmlrpc.client.ServerProxy("http://localhost:8001/")
+
+#invocamos la funcion "suma_remota" con el alias de "suma" desde el cliente y le pasamos los parametros a y b para mostrar el resultado en pantalla
+print("La suma es: " +str(proxy.suma(a,b)))
 
 
 
--- Servidor Sencillo/server.py
+++ Servidor Sencillo/server.py
@@ -1,14 +1,33 @@
-import xmlrpclib
-from SimpleXMLRPCServer import SimpleXMLRPCServer
+
+'''
+XML-RPC is a Remote Procedure Call method that uses XML passed via HTTP(S) as a transport. With it, a client can call methods with parameters on a remote server (the server is named by a URI) and get back structured data.
+
+The xmlrpc.server module provides a basic server framework for XML-RPC servers written in Python. Servers can either be free standing, using SimpleXMLRPCServer, or embedded in a CGI environment, using CGIXMLRPCRequestHandler.
+
+'''
+import xmlrpc.client
+
+from xmlrpc.server import SimpleXMLRPCServer
 
 def	suma_remota(a, b):
 	return a+b
 
 
-
+#se crea una nueva instancia de servidor 
 server = SimpleXMLRPCServer(("localhost", 8001))
+
+
+'''
+SimpleXMLRPCServer.register_function(function=None, name=None)
+Register a function that can respond to XML-RPC requests. If name is given, it will be the method name associated with function, otherwise function.__name__ will be used. 
+'''
+#registra la funcion para que pueda responder solicitudes XML-RPC del cliente  y le asigna el alias "suma" a la funcion
 server.register_function(suma_remota, "suma")
-print ("soy el servidor y estoy corriendo por el pueto 8001")
+
+#Imprimimos un mensaje en pantalla que indica que el servidor esta corriendo
+print ("soy el servidor y estoy corriendo por el puerto 8001")
+
+#iniciamos el servidor
 server.serve_forever()
 
 
