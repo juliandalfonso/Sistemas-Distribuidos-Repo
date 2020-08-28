adb084c67dcd48161d6bde6230454fe8565a9963
Julian Alfonso <guiaparalaensenanzadego@gmail.com>
2020-08-28 12:11:00 -0500

    se comentaron los archivos de Servidor Calculadora

--- Servidor Calculadora/cliente-menunew.py
+++ Servidor Calculadora/cliente-menunew.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 20 20:02:37 2018
+'''
+XML-RPC is a Remote Procedure Call method that uses XML passed via HTTP(S) as a transport. With it, a client can call methods with parameters on a remote server (the server is named by a URI) and get back structured data.
 
-@author: Cesar jaramillo
-
-"""
+'''
+#Importamos la libreria del lado del cliente XML_RPC y la libreria OS para limpiar pantalla
 import xmlrpc.client, os  
-#import xmlrpc.client, os
 
+#A ServerProxy instance is an object that manages communication with a remote XML-RPC server.The required first argument is a URI
+#Un proxy es un equipo informático que hace de intermediario entre las conexiones de un cliente y un servidor de destino
 s = xmlrpc.client.ServerProxy('http://localhost:8001')
+
+#creamos la funcion del menu
 def menu():
     print("**********MENU DE LLAMADAS A RPC*******************")
     print("1. SUMAR")
@@ -17,14 +18,19 @@ def menu():
     print("4 DIVIDIR")
     print("0 PARA SALIR")
 
+#corremos el menu
 menu()    
+
+
 while True:
+
     p=eval(input("digite su opcion: "))
     
    
     if p==1:
         x=int(eval(input("ingrese el primer numero: ")))
         y=int(eval(input("ingrese el primer numero: ")))
+        # invocamos las funciones de la clase funciones_rpc por medio del proxy creado previamente como "s"
         print(("el resultado de la suma es: "+str(s.suma(x,y))))
         a=eval(input())
     elif p==2:
@@ -32,7 +38,7 @@ while True:
         y=int(eval(input("ingrese el primer numero: ")))
         print(("el resultado de la resta es: "+str(s.resta(x,y))))
     elif p==3:
-        x=int(eval(input("ingrese el primer numero: ")))
+        x=int(eval(input("ingrese el primer numero: "))) 
         y=int(eval(input("ingrese el primer numero: ")))
         print(("el resultado de la multiplicacion es: "+str(s.multi(x,y))))
     elif p==4:
@@ -41,7 +47,7 @@ while True:
         print(("el resultado de la division es: "+str(s.divi(x,y))))
     elif p==0:
         print("que tengas un buen dia, hasta pronto")
-        break
+        break 
     else:
         os.system('cls')
      
--- Servidor Calculadora/server-clases.py
+++ Servidor Calculadora/server-clases.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 20 17:27:26 2020
-clase sistemas distribuidos
 
-@author: Usuario UTP
-"""
+'''
+XML-RPC is a Remote Procedure Call method that uses XML passed via HTTP(S) as a transport. With it, a client can call methods with parameters on a remote server (the server is named by a URI) and get back structured data.
+
+The xmlrpc.server module provides a basic server framework for XML-RPC servers written in Python. Servers can either be free standing, using SimpleXMLRPCServer, or embedded in a CGI environment, using CGIXMLRPCRequestHandler.
+
+'''
 from xmlrpc.server import SimpleXMLRPCServer
-#from xmlrpc.server import SimpleXMLRPCServer
 
+#creamos la clase que contendra las funciones a las cuales puede acceder el cliente
 class funciones_rpc:
     def suma(self, p, q):
         return p+q
@@ -19,8 +19,16 @@ class funciones_rpc:
     
     def divi(self, p, q):
         return p/q
+
+#Creamos el servidor en el puerto 8001
 server = SimpleXMLRPCServer(("localhost", 8001))
+
+#registramos la clase funciones_rpc en el servidor para darle acceso al proxy del cliente
 server.register_instance(funciones_rpc())
+
+#enviamos un mensaje comprobando que iniciamos el servidor 
 print("soy un servidor implementado con clases")
+
+#iniciamos el servidor
 server.serve_forever()
 
